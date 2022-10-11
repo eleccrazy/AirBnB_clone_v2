4b92d58d76f0b616aac971fd337caed9872edbef
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-11 16:37:27 +0300

    Task 8 completed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -46,6 +46,7 @@ class DBStorage:
             objs = self.__session.query(State).all()
             objs.extend(self.__session.query(City).all())
             objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
         else:
             if type(cls) == str:
                 cls = eval(cls)
