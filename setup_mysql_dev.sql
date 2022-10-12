673d9972f95a835c6e8cffb4254e48321977f2ed
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-12 21:49:28 +0300

    Commented perfectly

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create Database if doesn't Exist and set password for user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Grant Privileges to user
+-- Grant all Privileges to user on hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Grant select privileges to user on performa_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
